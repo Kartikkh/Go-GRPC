bd7e01f928299c6251390c4d9e158a817256661e
kartikkh <khandelwal.kartik4@gmail.com>
2019-09-18 12:39:52 +0530

    client streaming

--- greet/server/server.go
+++ greet/server/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/Go-GRPC/greet"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 	"net"
 	"strconv"
@@ -11,6 +12,25 @@ import (
 )
 
 type server struct{}
+
+func (s *server) LongGreet(stream greet.GreetService_LongGreetServer) error {
+	result := ""
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&greet.LongGreetResponse{
+				Result: result,
+			})
+		}
+		if err != nil {
+			log.Fatal("error while handling request", err)
+			return err
+		}
+		firstName := req.GetGreeting().FirstName
+		result = result + "Hello " + firstName + "!" +"\n"
+	}
+	return nil
+}
 
 func (s *server) Greet(ctx context.Context, req *greet.GreetRequest) (*greet.GreetResponse, error) {
 	firstName := req.GetGreeting().FirstName
@@ -21,7 +41,7 @@ func (s *server) Greet(ctx context.Context, req *greet.GreetRequest) (*greet.Gre
 }
 
 func (s *server) GreetManyTimes(req *greet.GreetManyTimesRequest, stream greet.GreetService_GreetManyTimesServer) error {
-	firstName := req.Greeting.GetFirstName()
+	firstName := req.GetGreeting().FirstName
 	for i := 0; i < 10; i++ {
 		result := "hello: " + firstName + "number " + strconv.Itoa(i)
 		res := &greet.GreetManyTimesResponse{
